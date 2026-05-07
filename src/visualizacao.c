1c7b9abd617754d4b8a2452c7c17dfb446342464
facalgaro01 <fabioaugustocalgaro@gmail.com>
2026-05-07 20:16:10 -0300

    Update visualizacao.c

--- src/visualizacao.c
+++ src/visualizacao.c
@@ -1,5 +1,35 @@
+#define _DEFAULT_SOURCE
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include "visualizacao.h"
+
+// Converte ISO 8601 UTC ("2025-07-01T10:39:20.153Z")
+// para "DD/MM/YYYY HH:MM:SS" no horario de Brasilia (UTC-3)
+static void formatar_data_hora(const char *iso, char *saida) {
+    if (!iso || iso[0] == '\0') {
+        strcpy(saida, "-");
+        return;
+    }
+
+    struct tm tm_utc = {0};
+    if (sscanf(iso, "%d-%d-%dT%d:%d:%d",
+               &tm_utc.tm_year, &tm_utc.tm_mon, &tm_utc.tm_mday,
+               &tm_utc.tm_hour, &tm_utc.tm_min, &tm_utc.tm_sec) != 6) {
+        strcpy(saida, iso);
+        return;
+    }
+    tm_utc.tm_year -= 1900;
+    tm_utc.tm_mon  -= 1;
+
+    time_t t = timegm(&tm_utc);
+    t -= 3 * 3600;
+
+    struct tm tm_local;
+    gmtime_r(&t, &tm_local);
+
+    strftime(saida, 20, "%d/%m/%Y %H:%M:%S", &tm_local);
+}
 
 void exibir_tracejado(int tipo, int tamanho) {
     char caractere = (tipo == 0) ? '-' : '=';
@@ -57,8 +87,12 @@ void exibir_tabelas(ESTATISTICAS *cidades, int qtd) {
                     media = cidades[i].pressao.soma / cidades[i].pressao.cont;
             }
 
+            char dMin_fmt[20], dMax_fmt[20];
+            formatar_data_hora(dMin, dMin_fmt);
+            formatar_data_hora(dMax, dMax_fmt);
+
             printf("%-21s | %-6.2f | %-22s | %-6.2f | %-22s | %-6.2f\n",
-                   cidades[i].nome_cidade, min, dMin, max, dMax, media);
+                   cidades[i].nome_cidade, min, dMin_fmt, max, dMax_fmt, media);
         }
         printf("\n");
     }
@@ -69,7 +103,10 @@ void exibir_tabelas(ESTATISTICAS *cidades, int qtd) {
     exibir_tracejado_padrao(0);
     for (int i = 0; i < qtd; i++) {
         printf("%-21s | %-11.2f | %-9.2f | %-11.2f\n",
-                cidades[i].nome_cidade, cidades[i].bateria.max.valor, cidades[i].bateria.min.valor, (cidades[i].bateria.max.valor - cidades[i].bateria.min.valor));
+                cidades[i].nome_cidade,
+                cidades[i].bateria_inicial.valor,
+                cidades[i].bateria_final.valor,
+                (cidades[i].bateria_inicial.valor - cidades[i].bateria_final.valor));
     }
     printf("\n");
 
